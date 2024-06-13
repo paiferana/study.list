e1e041107685a7ef36c2e248860245a585de9895
Ana Júlia Paifer <132628720+paiferana@users.noreply.github.com>
2024-06-13 18:32:16 -0300

    The project

---
+++ StudyList.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.studylist;
+
+import org.studylist.views.TelaInicial;
+
+/**
+ *
+ * @author Ana
+ */
+public class StudyList {
+
+    public static void main(String[] args) {
+        TelaInicial tl = new TelaInicial();
+        tl.setVisible(true);
+    }
+}
